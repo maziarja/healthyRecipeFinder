e31caae6f3eb087fbbaaaaa671d5d7f295ce44d9
maziarja <maziar.ja@gmail.com>
2026-01-17 14:55:28 -0800

    removing connect to the db

--- components/recipes/Recipes.tsx
+++ components/recipes/Recipes.tsx
@@ -1,8 +1,6 @@
-import connectDB from "@/lib/database";
 import RecipeFilters from "./RecipeFilters";
 
 async function Recipes() {
-  await connectDB();
   return (
     <section className="space-y-6 px-4 md:px-8 md:py-0 lg:mx-auto lg:max-w-314 lg:pb-24">
       <RecipeFilters />
