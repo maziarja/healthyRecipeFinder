f94225a53e470d0a41196676358aa778809806e8
maziarja <maziar.ja@gmail.com>
2026-01-22 15:05:15 -0800

    Adding User model

---
+++ models/User.ts
@@ -0,0 +1,26 @@
+import mongoose, { Model, models, Schema } from "mongoose";
+
+type UserType = {
+  email: string;
+  password: string;
+  fullName: string;
+};
+
+export const userSchema = new Schema<UserType>({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    minLength: [6, "Password must be at least 6 characters"],
+    required: [true, "Password is required"],
+  },
+  fullName: {
+    type: String,
+    required: [true, "Full name is required"],
+  },
+});
+
+export const User: Model<UserType> =
+  models.User || mongoose.model<UserType>("User", userSchema);
